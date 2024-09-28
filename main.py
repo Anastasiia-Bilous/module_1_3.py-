e6bece500cf03a6dceee5bec5bc11fa0b3948c49
Anastasiia-Bilous <ab5731141@gmail.com>
2024-09-28 18:57:48 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+name = "Anastasiia"# This is a sample Python script.
+print(name)
+age = 48# Press Shift+F10 to execute it or replace it with your code.
+print(age)# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+new_age = 1
+print(age + new_age)
+name = "is_student" # Use a breakpoint in the code line below to debug your script.
+print(name, type(name))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
